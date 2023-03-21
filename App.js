9c2e9cfb2fcda12c4ee67bf8d68cfa25e6a4ab4c
Blackmile <stanley.ezeifeanyi@gmail.com>
2023-03-22 00:36:11 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator();
+
+const SignUpStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name='SignIn' component={SignInScreen} options={{ headerShown: false }} />
+    </Stack.Navigator>
+  )
+}
+
+const MainStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name='Home' component={HomeScreen} />
+    </Stack.Navigator>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
