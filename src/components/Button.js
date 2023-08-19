9e3b6a7e603299d894e4ce8cd2a7850da3e67079
Blackmile <stanley.ezeifeanyi@gmail.com>
2023-08-19 11:28:49 +0100

    reuseable btn

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,22 +1,29 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
 
-const Button = () => {
+const Button = ({ text, backcolor, onPress }) => {
   return (
-    <View style={styles.Btn}>
-      <Text> send </Text>
-    </View>
+    <TouchableOpacity onPress={onPress}>
+      <View style={[styles.Btn, {backgroundColor: backcolor}]}>
+        <Text style={ styles.BtnText } > {text} </Text>
+      </View>
+    </TouchableOpacity>
+    
   )
 }
 
 const styles = StyleSheet.create({
     Btn: {
-        backgroundColor: 'rgba(168, 82, 55, 0.55)',
-        width: 50,
-        height: 20,
-        borderRadius: 2,
+        // backgroundColor: 'rgba(168, 82, 55, 0.55)',
+        width: 90,
+        height: 32.5,
+        borderRadius: 5,
         alignItems: 'center',
+        justifyContent: 'center',
         margin: 10,
+    },
+    BtnText: {
+      fontSize: 24,
     }
 })
 
